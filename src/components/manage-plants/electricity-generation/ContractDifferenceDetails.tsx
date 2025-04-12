a523efea0263637c0c04f520d13a8e8b2d0540a0
MaryemWannes <t-maryemw@greenearthx.com>
2025-04-12 18:42:57 +0100

    update manage plants details

--- src/components/manage-plants/electricity-generation/ContractDifferenceDetails.tsx
+++ src/components/manage-plants/electricity-generation/ContractDifferenceDetails.tsx
@@ -7,7 +7,7 @@ const ContractDifferenceDetails: React.FC = () => {
 
   return (
     <div className="ml-4 mt-2">
-      <label className="flex items-center gap-2">
+      <label className="flex items-center font-medium  gap-2">
         Provide CFD contract
       </label>
       <FileUpload label="Submit" onChange={setFile} />
