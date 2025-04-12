9ca2fc352b58a221f6098e8a160d06a600945a05
MaryemWannes <t-maryemw@greenearthx.com>
2025-04-12 18:46:35 +0100

    update manage plants details

--- src/components/manage-plants/electricity-generation/SelfGenerationDetails.tsx
+++ src/components/manage-plants/electricity-generation/SelfGenerationDetails.tsx
@@ -7,7 +7,7 @@ const SelfGenerationDetails: React.FC = () => {
 
   return (
     <div className="ml-4 mt-2">
-      <label className="flex items-center gap-2">Submit energy metering documents</label>
+      <label className="flex items-center font-medium gap-2">Submit energy metering documents</label>
       <FileUpload label="Submit" onChange={setFile} />
     </div>
   );
